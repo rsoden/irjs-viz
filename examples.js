b03428d6241858aa68f8a32f29dc75eb103e38c6
rsoden <robert.soden@gmail.com>
2015-02-23 17:42:40 -0700

    first commit

--- examples.js
+++ examples.js
@@ -1,41 +1,66 @@
-var domout = require('./lib/domout'),
-    viz = require('./lib/viz')
+var domout = require('./lib/domout')
+var viz = require('./lib/viz')
+var parse = require('csv-parse')
+var fs = require('fs')
 
-var data = [5, 10, 15, 20, 25]
+// Circles
+var circles = {}
+circles.options = {
+	'canvas-height' : '200',
+	'canvas-width' : '1500',
+	'stroke' : 'black',
+	'stroke-width' : '3',
+	'fill' :'red'
+}
 
-domout.html('<h1>Circles</h1>')
+circle_data = fs.readFileSync('./data/circle.csv','utf-8')
+parse(circle_data, function(err, res) {
+	if (err) { console.log(err) }
+	else if(res) {
+		circles.data = res
+		domout.html('<h1>Circles</h1>')
+		domout.svg(viz.circles(circles))
+	}
+})
 
-// note: use domout.svg for <svg> tags for them to be displayed properly
-domout.svg(viz.circles(data))
+// Barcharts
+var barchart = {}
+barchart.options = {
+	'canvas-height' : '200',
+	'canvas-width' : '1500',
+	'stroke' : 'black',
+	'stroke-width' : '3',
+	'fill' :'red',
+	'title' : 'Test Bar Chart'
+}
 
-domout.html('<h1>Barchart</h1>')
-domout.svg(viz.barchart(data, {
-    title: 'some title'
-}))
+barchart_data = fs.readFileSync('./data/barchart.csv','utf-8')
+parse(barchart_data, function(err, res) {
+	if (err) { console.log(err) }
+	else if(res) {
+		barchart.data = res
+		domout.html('<h1>Barchart</h1>')
+		domout.svg(viz.barchart(barchart))
+	}
+})
 
-domout.html('<h1>Line Graph</h1>')
-domout.svg(viz.linegraph(data))
+// Linegraph
+var linegraph = {}
+linegraph.options = {
+	'canvas-height' : '200',
+	'canvas-width' : '1500',
+	'stroke' : 'black',
+	'stroke-width' : '3',
+	'fill' :'red',
+	'title' : 'Linegraph'
+}
 
-domout.html('<h1>List</h1>')
-domout.html(viz.list(data))
-
-domout.html('<h1>JSON</h1>')
-var data = [
-    ['John', 3, 5],
-    ['Adam', 4, 8]
-]
-
-domout.html(viz.json(data))
-
-domout.html('<h1>Table</h1>')
-domout.html(viz.table(data))
-
-
-var data = [
-    [5, 5],
-    [10, 10],
-    [15, 15]
-]
-
-domout.html('<h1>Scatter Plot</h1>')
-domout.svg(viz.scatterplot(data))
+linegraph_data = fs.readFileSync('./data/linegraph.csv','utf-8')
+parse(linegraph_data, function(err, res) {
+	if (err) { console.log(err) }
+	else if(res) {
+		linegraph.data = res
+		domout.html('<h1>Linegraph</h1>')
+		domout.svg(viz.linegraph(linegraph))
+	}
+})
